5c4562d45aff78ed4ec8e2199251e4a7eba3c6a2
Fahmi <fahmiardiyannto@yahoo.com>
2023-12-20 17:23:51 +0700

    Setup Project with SwiftUI

---
+++ MovieDB/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieDB
+//
+//  Created by Muhammad Fahmi on 20/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieDB/MovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBApp.swift
+//  MovieDB
+//
+//  Created by Muhammad Fahmi on 20/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
