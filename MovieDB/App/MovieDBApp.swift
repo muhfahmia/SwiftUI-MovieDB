d4027a09d90b1b5fd53f4d8011dd4f250f8585e8
Fahmi <fahmiardiyannto@yahoo.com>
2023-12-30 14:45:26 +0700

    Setup Pod for Depedency Manager

--- MovieDB/App/MovieDBApp.swift
+++ MovieDB/App/MovieDBApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct MovieDBApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            OnBoard()
+                .environment(\.colorScheme, .light)
         }
     }
 }
---
+++ MovieDB/Core/Data/DataSource/Remote/RemoteDataSource.swift
@@ -0,0 +1,10 @@
+//
+//  RemoteDataSource.swift
+//  MovieDB
+//
+//  Created by Muhammad Fahmi on 28/12/23.
+//
+
+import Foundation
+
+
